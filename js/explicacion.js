3e92e4a7ce7c5041ad954427820d685ba6e82e2f
Juan Jose <jjgasa01@gmail.com>
2022-05-18 12:28:56 -0500

    subo toda la pagina

---
+++ js/explicacion.js
@@ -0,0 +1,6 @@
+const cuadro = document.getElementById('cuadro3')
+const btn = document.getElementById('btn_mover')
+btn.onclick = function mover(){
+	cuadro.classList.toggle('mover')
+}
+console.log("prueba2")
